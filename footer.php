2fe87f16fc0138a2971e79900c23693256804b26
Martijn <martijn.vervoort99@gmail.com>
2025-03-14 16:08:24 +0100

    Aanpassen van CSS en font paden voor nieuwe mappenstructuur

---
+++ footer.php
@@ -0,0 +1,36 @@
+</main> <!-- Sluit de main sectie of content -->
+
+<footer>
+    <div class="footer-content" style="margin-top: 50px;">
+        <div class="fc-1">
+            <div class="bb-footer-logo"></div>
+            <div class="f-bb" style="width: 400px;">
+                <h4 style="font-family: 'Cabin'">BOES branding</h4>
+                <p>De ontdekkingsreis naar je merk.<br>
+                KVK: 76315495</p>
+            </div>
+        </div>
+        <div class="fc-2">
+            <div class="f-contact">
+                <h4 style="font-family: 'Cabin'">Contact</h4>
+                <p>+31 6 29799993 <br>
+                info@boesbranding.com</p>
+            </div>
+            <div class="f-social">
+                <h4 style="font-family: 'Cabin'">Social media:</h4>
+                <div class="f-social-images">
+                    <a href="https://www.instagram.com/boesbranding" target="blank" class="f-social-img1"></a>
+                    <a href="https://www.facebook.com/boesbranding" target="blank" class="f-social-img2"></a>
+                    <a href="https://www.linkedin.com/in/nina-boes-boesbranding-branddesigner-grafisch-ontwerper-merkidentiteiten-branding/?originalSubdomain=nl" target="blank" class="f-social-img3"></a>
+                    <a href="tel:+31629799993" class="f-social-img4"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <p style="color: #525252; text-align: center;">&copy; <?php echo date("Y"); ?> BOES Branding. Alle rechten voorbehouden.</p>
+</footer>
+
+<?php wp_footer(); // Zorgt ervoor dat plugins en scripts worden geladen ?>
+
+</body>
+</html>
